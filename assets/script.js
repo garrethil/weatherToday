04898988bf274917121164c7c63b96c43ff69ddf
garrethil <garrethildebrandt@gmail.com>
2024-01-01 23:19:56 -0500

    add day.js to js to show correct dates

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,17 @@ var temp = $('.temp');
 var windSpeed = $('.windSpeed');
 var humidity = $('.humidity');
 var weatherIcon = $('.icon');
+var date = $('.date');
+var day1 = $('#day1');
+var day2 = $('#day2');
+var day3 = $('#day3');
+var day4 = $('#day4');
+var day5 = $('#day5');
 var apiKey = 'b2783bbf1219f33d7587949eaf4936eb'
 var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 var weekForcastUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
+const currentDate = dayjs();
+
 
 function searchFormSubmit (event){
     event.preventDefault();
@@ -23,15 +31,14 @@ function searchFormSubmit (event){
 
 // Store value in a key value to be stored in local storage
 
-
-// use fetch() to get info from the Open weather API
 async function getWeather() {
     const location = $.trim(inputText.val());
+
     const infoCall = await fetch(apiUrl + `&q=${location}` + `&appid=${apiKey}`);
     var infoReturn = await infoCall.json();
-
     const weekForcastCall = await fetch(weekForcastUrl + `&q=${location}` + `&appid=${apiKey}`);
     var weekForcastReturn = await weekForcastCall.json();
+
     city.html(infoReturn.name);
     temp.html(Math.round(infoReturn.main.temp) + '°C');
     windSpeed.html(Number(infoReturn.wind.speed.toFixed(1)) + ' km/h');
@@ -51,14 +58,39 @@ async function getWeather() {
         weatherIcon.attr('src', './assets/images/snow.png');
     }
     
-    console.log(weekForcastReturn);
+
+
+
+
+
+
 }
 
 
 // create elements to display the weather information
 
 
+var checkTime = function() {
 
+date.html(currentDate.format('dddd, MMMM D, YYYY'));
+
+const day1Name = currentDate.add(1, 'day');
+const day2Name = currentDate.add(2, 'day');
+const day3Name = currentDate.add(3, 'day');
+const day4Name = currentDate.add(4, 'day');
+const day5Name = currentDate.add(5, 'day');
+
+day1.html(day1Name.format('ddd'));
+day2.html(day2Name.format('ddd'));
+day3.html(day3Name.format('ddd'));
+day4.html(day4Name.format('ddd'));
+day5.html(day5Name.format('ddd'));
+
+}
+
+
+
+checkTime();
 
 
 
