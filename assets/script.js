3f79112383fd81f4a29cbcadad9898abef108f73
garrethil <garrethildebrandt@gmail.com>
2024-01-01 22:44:08 -0500

    correct icon switching and call for 5 day weather forcast

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,9 @@ var temp = $('.temp');
 var windSpeed = $('.windSpeed');
 var humidity = $('.humidity');
 var weatherIcon = $('.icon');
-var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=b2783bbf1219f33d7587949eaf4936eb";
+var apiKey = 'b2783bbf1219f33d7587949eaf4936eb'
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+var weekForcastUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 
 function searchFormSubmit (event){
     event.preventDefault();
@@ -25,30 +27,31 @@ function searchFormSubmit (event){
 // use fetch() to get info from the Open weather API
 async function getWeather() {
     const location = $.trim(inputText.val());
-    const infoCall = await fetch(apiUrl + `&q=${location}`);
+    const infoCall = await fetch(apiUrl + `&q=${location}` + `&appid=${apiKey}`);
     var infoReturn = await infoCall.json();
 
+    const weekForcastCall = await fetch(weekForcastUrl + `&q=${location}` + `&appid=${apiKey}`);
+    var weekForcastReturn = await weekForcastCall.json();
     city.html(infoReturn.name);
     temp.html(Math.round(infoReturn.main.temp) + '°C');
     windSpeed.html(Number(infoReturn.wind.speed.toFixed(1)) + ' km/h');
     humidity.html(infoReturn.main.humidity + ' % humidity');
 
     if (infoReturn.weather[0].main == 'Clouds') {
-        weatherIcon.src = "images/clouds.png";
+        weatherIcon.attr('src', './assets/images/clouds.png');
     }else if (infoReturn.weather[0].main == 'Clear') {
-        weatherIcon.src = "images/clear.png";
+        weatherIcon.attr('src', './assets/images/clear.png');
     }else if (infoReturn.weather[0].main == 'Rain') {
-        weatherIcon.src = "images/rain.png";
+        weatherIcon.attr('src', './assets/images/rain.png');
     }else if (infoReturn.weather[0].main == 'Drizzle') {
-        weatherIcon.src = "images/drizzle.png";
+        weatherIcon.attr('src', './assets/images/drizzle.png');
     }else if (infoReturn.weather[0].main == 'Mist') {
-        weatherIcon.src = "images/mist.png";
+        weatherIcon.attr('src', './assets/images/mist.png');
     }else if (infoReturn.weather[0].main == 'Snow') {
-        weatherIcon.src = "images/snow.png";
+        weatherIcon.attr('src', './assets/images/snow.png');
     }
     
-    console.log(infoReturn);
-
+    console.log(weekForcastReturn);
 }
 
 
