b4218a0c81ef8c84d817aee47f425c4203ef08f1
garrethil <garrethildebrandt@gmail.com>
2024-01-02 13:53:09 -0500

    display weather info for 5 day forcast from openweather

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,12 @@ var day2 = $('#day2');
 var day3 = $('#day3');
 var day4 = $('#day4');
 var day5 = $('#day5');
-var apiKey = 'b2783bbf1219f33d7587949eaf4936eb'
+var icon1 = $('#1Icon');
+var icon2 = $('#2Icon');
+var icon3 = $('#3Icon');
+var icon4 = $('#4Icon');
+var icon5 = $('#5Icon');
+var apiKey = 'b2783bbf1219f33d7587949eaf4936eb';
 var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 var weekForcastUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 const currentDate = dayjs();
@@ -44,6 +49,24 @@ async function getWeather() {
     windSpeed.html(Number(infoReturn.wind.speed.toFixed(1)) + ' km/h');
     humidity.html(infoReturn.main.humidity + ' % humidity');
 
+    $('.temp1').html(Math.round(weekForcastReturn.list[0].main.temp) + '°C');
+    $('.temp2').html(Math.round(weekForcastReturn.list[1].main.temp) + '°C');
+    $('.temp3').html(Math.round(weekForcastReturn.list[2].main.temp) + '°C');
+    $('.temp4').html(Math.round(weekForcastReturn.list[3].main.temp) + '°C');
+    $('.temp5').html(Math.round(weekForcastReturn.list[4].main.temp) + '°C');
+
+    $('.windSpeed1').html(Number(weekForcastReturn.list[0].wind.speed.toFixed(1)) + ' km/h');
+    $('.windSpeed2').html(Number(weekForcastReturn.list[1].wind.speed.toFixed(1)) + ' km/h');
+    $('.windSpeed3').html(Number(weekForcastReturn.list[2].wind.speed.toFixed(1)) + ' km/h');
+    $('.windSpeed4').html(Number(weekForcastReturn.list[3].wind.speed.toFixed(1)) + ' km/h');
+    $('.windSpeed5').html(Number(weekForcastReturn.list[4].wind.speed.toFixed(1)) + ' km/h');
+
+    $('.humidity1').html('humidity ' + weekForcastReturn.list[0].main.humidity + '%');
+    $('.humidity2').html('humidity ' + weekForcastReturn.list[1].main.humidity  + '%');
+    $('.humidity3').html('humidity ' + weekForcastReturn.list[2].main.humidity  + '%');
+    $('.humidity4').html('humidity ' + weekForcastReturn.list[3].main.humidity  + '%');
+    $('.humidity5').html('humidity ' + weekForcastReturn.list[4].main.humidity  + '%');
+
     if (infoReturn.weather[0].main == 'Clouds') {
         weatherIcon.attr('src', './assets/images/clouds.png');
     }else if (infoReturn.weather[0].main == 'Clear') {
@@ -57,17 +80,94 @@ async function getWeather() {
     }else if (infoReturn.weather[0].main == 'Snow') {
         weatherIcon.attr('src', './assets/images/snow.png');
     }
+
+    // 1
+
+    if (weekForcastReturn.list[0].weather[0].main == 'Clouds') {
+        icon1.attr('src', './assets/images/clouds.png');
+    }else if (weekForcastReturn.list[0].weather[0].main == 'Clear') {
+        icon1.attr('src', './assets/images/clear.png');
+    }else if (weekForcastReturn.list[0].weather[0].main == 'Rain') {
+        icon1.attr('src', './assets/images/rain.png');
+    }else if (weekForcastReturn.list[0].weather[0].main == 'Drizzle') {
+        icon1.attr('src', './assets/images/drizzle.png');
+    }else if (weekForcastReturn.list[0].weather[0].main == 'Mist') {
+        icon1.attr('src', './assets/images/mist.png');
+    }else if (weekForcastReturn.list[0].weather[0].main == 'Snow') {
+        icon1.attr('src', './assets/images/snow.png');
+    }
     
+// 2
+
+    if (weekForcastReturn.list[1].weather[0].main == 'Clouds') {
+        icon2.attr('src', './assets/images/clouds.png');
+    }else if (weekForcastReturn.list[1].weather[0].main == 'Clear') {
+        icon2.attr('src', './assets/images/clear.png');
+    }else if (weekForcastReturn.list[1].weather[0].main == 'Rain') {
+        icon2.attr('src', './assets/images/rain.png');
+    }else if (weekForcastReturn.list[1].weather[0].main == 'Drizzle') {
+        icon2.attr('src', './assets/images/drizzle.png');
+    }else if (weekForcastReturn.list[1].weather[0].main == 'Mist') {
+        icon2.attr('src', './assets/images/mist.png');
+    }else if (weekForcastReturn.list[1].weather[0].main == 'Snow') {
+        icon2.attr('src', './assets/images/snow.png');
+    }
+
+// 3
+
+    if (weekForcastReturn.list[2].weather[0].main == 'Clouds') {
+        icon3.attr('src', './assets/images/clouds.png');
+    }else if (weekForcastReturn.list[2].weather[0].main == 'Clear') {
+        icon3.attr('src', './assets/images/clear.png');
+    }else if (weekForcastReturn.list[2].weather[0].main == 'Rain') {
+        icon3.attr('src', './assets/images/rain.png');
+    }else if (weekForcastReturn.list[2].weather[0].main == 'Drizzle') {
+        icon3.attr('src', './assets/images/drizzle.png');
+    }else if (weekForcastReturn.list[2].weather[0].main == 'Mist') {
+        icon3.attr('src', './assets/images/mist.png');
+    }else if (weekForcastReturn.list[2].weather[0].main == 'Snow') {
+        icon3.attr('src', './assets/images/snow.png');
+    }
+
+    // 4
+
+    if (weekForcastReturn.list[3].weather[0].main == 'Clouds') {
+        icon4.attr('src', './assets/images/clouds.png');
+    }else if (weekForcastReturn.list[3].weather[0].main == 'Clear') {
+        icon4.attr('src', './assets/images/clear.png');
+    }else if (weekForcastReturn.list[3].weather[0].main == 'Rain') {
+        icon4.attr('src', './assets/images/rain.png');
+    }else if (weekForcastReturn.list[3].weather[0].main == 'Drizzle') {
+        icon4.attr('src', './assets/images/drizzle.png');
+    }else if (weekForcastReturn.list[3].weather[0].main == 'Mist') {
+        icon4.attr('src', './assets/images/mist.png');
+    }else if (weekForcastReturn.list[3].weather[0].main == 'Snow') {
+        icon4.attr('src', './assets/images/snow.png');
+    }
+
+    // 5
+
+    if (weekForcastReturn.list[4].weather[0].main == 'Clouds') {
+        icon5.attr('src', './assets/images/clouds.png');
+    }else if (weekForcastReturn.list[4].weather[0].main == 'Clear') {
+        icon5.attr('src', './assets/images/clear.png');
+    }else if (weekForcastReturn.list[4].weather[0].main == 'Rain') {
+        icon5.attr('src', './assets/images/rain.png');
+    }else if (weekForcastReturn.list[4].weather[0].main == 'Drizzle') {
+        icon5.attr('src', './assets/images/drizzle.png');
+    }else if (weekForcastReturn.list[4].weather[0].main == 'Mist') {
+        icon5.attr('src', './assets/images/mist.png');
+    }else if (weekForcastReturn.list[4].weather[0].main == 'Snow') {
+        icon5.attr('src', './assets/images/snow.png');
+    }
 
 
 
 
-
+console.log(weekForcastReturn);
+console.log(weekForcastReturn.list[0].weather[0].main);
 
 }
-
-
-// create elements to display the weather information
 
 
 var checkTime = function() {
